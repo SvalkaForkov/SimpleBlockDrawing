2a305fb80598c42a6a24a501422bd9c4e584896a
mhergon <mhergon@gmail.com>
2014-02-26 16:43:49 +0100

    - Firsr commit

---
+++ UIView+DrawBlock.h
@@ -0,0 +1,19 @@
+//
+//  UIView+DrawRectBlock.h
+//  TestDrawRect
+//
+//  Created by mhergon on 25/02/14.
+//  Copyright (c) 2014 mhergon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// Blocks
+typedef void(^DrawBlock)(CGContextRef context);
+typedef void(^CompletionBlock)(UIView *view);
+
+@interface UIView (DrawBlock)
+
+- (void)drawInside:(DrawBlock)block withResult:(CompletionBlock)completion;
+
+@end
---
+++ UIView+DrawBlock.m
@@ -0,0 +1,73 @@
+//
+//  UIView+DrawRectBlock.m
+//  TestDrawRect
+//
+//  Created by mhergon on 25/02/14.
+//  Copyright (c) 2014 mhergon. All rights reserved.
+//
+
+#import "UIView+DrawBlock.h"
+#import <objc/runtime.h>
+
+#pragma mark - Auxiliar view
+@interface DrawingView : UIView
+
+@property (copy, nonatomic) DrawBlock drawBlock;
+
+- (void)setDrawRectBlock:(DrawBlock)block;
+
+@end
+
+@implementation DrawingView
+- (void)setDrawRectBlock:(DrawBlock)block {
+    if((_drawBlock = [block copy])) {
+        [self setNeedsDisplay];
+    }
+}
+
+- (void)drawRect:(CGRect)rect {
+    if(_drawBlock) {
+        //_drawBlock(self, rect);
+        _drawBlock(UIGraphicsGetCurrentContext());
+    }
+}
+
+- (void)dealloc {
+    _drawBlock = nil;
+}
+
+@end
+
+#pragma mark - Category
+@interface UIView ()
+
+@property DrawBlock drawBlock;
+
+@end
+
+@implementation UIView (DrawBlock)
+
+- (void)drawInside:(DrawBlock)block withResult:(CompletionBlock)completion {
+    if ((self.drawBlock = [block copy])) {
+        DrawingView *drawView = [[DrawingView alloc] initWithFrame:self.bounds];
+        drawView.backgroundColor = [UIColor clearColor];
+        [drawView setDrawRectBlock:self.drawBlock];
+        [self addSubview:drawView];
+        completion(drawView);
+    }
+}
+
+- (void)dealloc {
+    self.drawBlock = nil;
+}
+
+#pragma mark - Others
+- (void)setDrawBlock:(DrawBlock)drawBlock {
+    objc_setAssociatedObject(self, @"block", drawBlock, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+- (DrawBlock)drawBlock {
+    return objc_getAssociatedObject(self, @"block");
+}
+
+@end
