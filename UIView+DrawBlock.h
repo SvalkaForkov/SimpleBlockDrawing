877b712ec6d031a691958f32334fbe879c60a9ec
mhergon <mhergon@gmail.com>
2014-06-20 15:58:39 +0200

    - Added rect

--- UIView+DrawBlock.h
+++ UIView+DrawBlock.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 // Blocks
-typedef void(^DrawBlock)(CGContextRef context, CGrect rect);
+typedef void(^DrawBlock)(CGContextRef context, CGRect rect);
 typedef void(^CompletionBlock)(UIView *view);
 
 @interface UIView (DrawBlock)
